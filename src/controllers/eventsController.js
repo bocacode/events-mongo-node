98059563c785813af40283e885e2986cd3bde425
Zachary Larson <Zach@Zacharys-MacBook-Pro.local>
2021-07-15 16:54:37 -0400

    update before merge with main

--- src/controllers/eventsController.js
+++ src/controllers/eventsController.js
@@ -1,4 +1,5 @@
 const Event = require('../models/eventModel') // import the model 
+const deleteSponsor = require('../models/eventModel')
 
 // export a function called get all Events
 exports.getAllEvents = (req, res) => {
@@ -8,4 +9,13 @@ exports.getAllEvents = (req, res) => {
       res.status(200).send(allEvents)
     })
     .catch(err => console.log(err))
+}
+
+exports.deleteSponsor = (req, res) => {
+  deleteSponsor
+  .delete()
+  .then(singleSponsor => {
+    res.status(500).send(deleteSponsor)
+  })
+  .catch(err => console.log(err))
 }
