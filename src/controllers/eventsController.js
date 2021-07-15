73bd2203b1b44801245451ffc14e0133e321b6e1
luizsilva <luizfsilva.091595@gmail.com>
2021-07-15 17:30:52 -0400

    added get event by name

--- src/controllers/eventsController.js
+++ src/controllers/eventsController.js
@@ -1,9 +1,8 @@
-const Event = require('../models/eventModel') // import the model 
+const Event = require('../models/eventModel') // import the model
 
 // export a function called get all Events
 exports.getAllEvents = (req, res) => {
-  Event
-    .find()
+  Event.find()
     .then(allEvents => {
       res.status(200).send(allEvents)
     })
@@ -12,7 +11,11 @@ exports.getAllEvents = (req, res) => {
 
 // export a function that gets event by name
 exports.getEventByName = (req, res) => {
-  Event.find()
-  .then()
-  .catch(err => res.status(500).send('Could not find event'))
+  const { eventName } = req.query
+  Event.findOne({ title: eventName })
+    .then(event => {
+      console.log(event)
+      res.send(event)
+    })
+    .catch(err => res.status(500).send('Could not find event'))
 }
