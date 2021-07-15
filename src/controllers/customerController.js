0664d2eac31b2b9dc3091f47a3bcc3a6c9f5fe72
Matthew McKney <matthewmckney@Matthews-MacBook-Pro.local>
2021-07-15 19:12:06 -0400

    added customerController

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -1,8 +1,15 @@
 const Customer = require('../models/customerModel')
-const express = require('express')
-// const app = express()
-// app.use(express.json())
+
 
 exports.updateCustomer = (req, res) => {
-    Customer.findOneAndUpdate()
+    Customer
+    .find(req.body)
+    .findOneAndUpdate(req.params.fName, { 
+        $set: {
+            fName: "Timmy"
+        }
+    }, {new: true})
+    .then(res.status(200).send('created', Customer))
+    .catch(err => console.log(err))
+
 }
