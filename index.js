743d9da865e9917d9813c797ef45c896c20c68e7
Todd Albert <todd@bocacode.com>
2021-07-15 11:45:36 -0400

    event was created

--- index.js
+++ index.js
@@ -11,9 +11,35 @@ mongoose
   .then(() => console.log('connected to mongo...'))
   .catch(err => console.log(err))
 
+//create schema = rules to control type of data
+const eventSchema = mongoose.Schema({
+  title: String,
+  date: Date,
+  description: String,
+  cost: String,
+  attendees: Array
+})
+
+const Event = mongoose.model('Event', eventSchema) // create model from Schema
+
+const newEvent = {
+  title: 'Clean up Boca Beach',
+  date: '2021-08-02',
+  description: 'Cleaning up all trash from Boca beach',
+  cost: 'Free',
+  attendees: ['Christian', 'Mia', 'Noah', 'Emily', 'Zack']
+}
+
+function createEvent() {
+  new Event(newEvent)
+    .save()
+    .then(() => console.log('event was saved'))
+    .catch(err => console.log(err))
+}
 
 app.get('/', (req, res) => {
-  res.send('Hello Class')
+  createEvent()
+  res.send('Hello Class, event was created')
 })
 
 app.listen(5000, (req, res) => {
