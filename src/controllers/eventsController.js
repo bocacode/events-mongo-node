3d56ad3b05e0d06b08c168f28445d2d497bfb477
Mia Sanchez <mia@Claudias-MacBook-Air.local>
2021-07-15 16:57:18 -0400

    delete sponser updates

--- src/controllers/eventsController.js
+++ src/controllers/eventsController.js
@@ -1,4 +1,5 @@
 const Event = require('../models/eventModel') // import the model 
+const deleteSponsors = require('../model/eventModel')
 
 // export a function called get all Events
 exports.getAllEvents = (req, res) => {
@@ -8,4 +9,13 @@ exports.getAllEvents = (req, res) => {
       res.status(200).send(allEvents)
     })
     .catch(err => console.log(err))
+}
+
+exports.deleteSingleSponsor = (req, res) => {
+ deleteSponsor
+  .delete()
+  .then( SingleSponsor =>  {
+  res.status(500).send(deleteSingleSponsor)
+})
+.catch(err => console.log(err))
 }
