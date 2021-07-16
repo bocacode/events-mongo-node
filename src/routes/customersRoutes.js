8c0deaa5455e8bd467dedd41fe25fcff8aa474c6
Matthew McKney <matthewmckney@Matthews-MacBook-Pro.local>
2021-07-16 11:26:17 -0400

    added getone customer

--- src/routes/customersRoutes.js
+++ src/routes/customersRoutes.js
@@ -6,5 +6,6 @@ const customersController = require('../controllers/customersController')
 router.post('/addCustomer', customersController.createCustomer)
 router.patch('/customer/:fName', customersController.updateCustomer)
 router.get('/customers', customersController.getAllCustomers)
+router.get('/customer/:fName', customersController.getCustomerByName)
 
 module.exports = router
