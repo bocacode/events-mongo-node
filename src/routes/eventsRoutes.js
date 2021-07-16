0a734e2c5472ed61f9371774d2188c15d7d2ba80
luizsilva <luizfsilva.091595@gmail.com>
2021-07-16 11:42:13 -0400

    added an event delete feature

--- src/routes/eventsRoutes.js
+++ src/routes/eventsRoutes.js
@@ -4,7 +4,9 @@ const router = express.Router() // get router function from express
 const eventsController = require('../controllers/eventsController')
 
 router.get('/events/bydate/:search', eventsController.getEventByDate)
-router.get('/events/byname/:search', eventsController.getEventByName)
+router.get('/events/bytitle/:search', eventsController.getEventByTitle)
 router.get('/events', eventsController.getAllEvents)
+
+router.delete('/events/:eventTitle', eventsController.deleteEvent)
 
 module.exports = router
