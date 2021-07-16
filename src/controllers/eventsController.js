30646f8340c25fadb86a6fae7985393475b9b29e
chrisdeleonn <cdizzel@ymail.com>
2021-07-16 09:09:14 -0400

    created addSponsor branch

--- src/controllers/eventsController.js
+++ src/controllers/eventsController.js
@@ -1,11 +1,10 @@
-const Event = require('../models/eventModel') // import the model 
+const Event = require("../models/eventModel") // import the model
 
 // export a function called get all Events
 exports.getAllEvents = (req, res) => {
-  Event
-    .find()
-    .then(allEvents => {
+  Event.find()
+    .then((allEvents) => {
       res.status(200).send(allEvents)
     })
-    .catch(err => console.log(err))
+    .catch((err) => console.log(err))
 }
