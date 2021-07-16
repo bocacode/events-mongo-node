c3281b1894fde01efde724f2e9ea2d074ada4ddc
Matthew McKney <matthewmckney@Matthews-MacBook-Pro.local>
2021-07-16 08:32:06 -0400

    getting closer on customercontroller

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -3,7 +3,7 @@ const Customer = require('../models/customerModel')
 
 exports.updateCustomer = (req, res) => {
     Customer
-    .findOneAndUpdate(req.parmas.id, {$set: req.body}, {new: true})
+    .findOneAndUpdate('customers', {$set: req.body}, {new: true})
     .then(res.status(200).send('updated', Customer))
     .catch(err => console.log(err))
 
